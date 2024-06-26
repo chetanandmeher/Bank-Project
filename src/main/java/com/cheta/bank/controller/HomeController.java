9067fdc5d6033050120bd16d90f9aca953c03456
Chetanand Meher <chetanandmeher1282004@gmail.com>
2024-06-26 19:12:11 +0530

    Initial commit

---
+++ src/main/java/com/cheta/bank/controller/HomeController.java
@@ -0,0 +1,58 @@
+package com.cheta.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String login() {
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUp() {
+        return "signup";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword() {
+        return "forgot-password";
+    }
+
+    @GetMapping("/tables")
+    public String tables() {
+        return "tables";
+    }
+
+    @GetMapping("/users-table")
+    public String UsersTable() {
+        return "users-table";
+    }
+
+    @GetMapping("/user-form")
+    public String UserForm() {
+        return "user-form";
+    }
+
+    @GetMapping("/accounts-table")
+    public String AccountsTable() {
+        return "accounts-table";
+    }
+
+    @GetMapping("/state-table")
+    public String StateTable() {
+        return "state-table";
+    }
+
+    @GetMapping("/rough")
+    public String AccountForm() {
+        return "rough";
+    }
+}
